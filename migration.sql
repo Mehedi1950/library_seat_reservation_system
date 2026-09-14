d2778dcbd0e8215066a3cd9ef378c35f17798734
Mehedi1950 <117599561+Mehedi1950@users.noreply.github.com>
2026-09-15 00:47:42 +0600

    Add files via upload

---
+++ migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "zones" ADD COLUMN     "rules" TEXT[] DEFAULT ARRAY[]::TEXT[];
+
+-- CreateTable
+CREATE TABLE "settings" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "description" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "settings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "settings_key_key" ON "settings"("key");
